97540f23ad47f745331171f6ffe76d509b2bf243
kasthuriamsan <30368665+kasthuriamsan@users.noreply.github.com>
2017-07-26 17:59:19 +0530

    Create kkk.c

---
+++ kkk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ int number;
+ printf("enter a number");
+ scanf("%d",&n);
+ if(num %2==0)
+ printf(" %d is   even",num);
+ else
+ printf(" %d is odd",num);
+ return o;
+ }
+ 
